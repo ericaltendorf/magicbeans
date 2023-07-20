b0b6ed66182c903c68f5d86422f57802359eeae9
Eric <ericaltendorf@gmail.com>
2023-07-19 22:06:53 -0700

    Fix costspec generator

--- common.py
+++ common.py
@@ -2,7 +2,10 @@ from beancount.core import position
 
 # Need to add a USD cost spec on transfers, see
 #   https://github.com/beancount/beancount/issues/476
-def usd_cost_spec():
+def usd_cost_spec(transferred_currency):
+    if transferred_currency == "USD":
+        return None  # No cost basis needed to track
+    
     return position.CostSpec(None, None, "USD", None, None, None)
     # return position.Cost(None, "USD", None, None)
     # return None

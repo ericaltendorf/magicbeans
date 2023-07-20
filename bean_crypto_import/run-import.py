7e8d697b3ee39563ebb7f8f88c202dafa2c38a32
Eric <ericaltendorf@gmail.com>
2023-07-20 12:36:11 -0700

    Start structuring as an actual python package

---
+++ bean_crypto_import/run-import.py
@@ -0,0 +1,9 @@
+"""Run importers and reconcile data, using the beancount-import framework
+   (https://github.com/jbms/beancount-import)."""
+
+import glob
+import os
+import json
+import sys
+
+import importer_chiawallet

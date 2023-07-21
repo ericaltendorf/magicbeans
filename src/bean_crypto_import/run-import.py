89227d01a91a57d3ebc9f7190a34a085e21578ff
Eric <ericaltendorf@gmail.com>
2023-07-20 20:13:27 -0700

    filling out the beancount-importer script

--- src/bean_crypto_import/run-import.py
+++ src/bean_crypto_import/run-import.py
@@ -6,13 +6,44 @@ import os
 import json
 import sys
 
+from bean_crypto_import.importers.coinbase import CoinbaseImporter
+
+# TODO: factor this out
+cb_importer = CoinbaseImporter(
+   account_root="Assets:Coinbase",
+   account_external_root="Assets:ALLEXTERNAL",
+   account_gains="Income:PnL",
+   account_fees="Expenses:Financial:Fees",
+)
 
 def run_reconcile(extra_args):
-   # import beancount_import.webserver
+   import beancount_import.webserver
 
    journal_dir = os.path.dirname(__file__)
    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
 
-   data_sources = [ ]
+   data_sources = [ 
+       dict(
+         module='beancount_import.source.generic_importer_source',
+         importer=cb_importer,
+         directory='/home/eric/crypto-taxes/downloads/Coinbase',
+       )
+   ]
 
-   # beancount_import.webserver ...
+   beancount_import.webserver.main(
+      extra_args,
+      journal_input=os.path.join(journal_dir, 'journal.beancount'),
+      ignored_journal=os.path.join(journal_dir, 'ignored.beancount'),
+      default_output=os.path.join(journal_dir, 'transactions.beancount'),
+      open_account_output_map=[
+         ('.*', os.path.join(journal_dir, 'accounts.beancount')),
+      ],
+      balance_account_output_map=[
+         ('.*', os.path.join(journal_dir, 'accounts.beancount')),
+      ],
+      price_output=os.path.join(journal_dir, 'prices.beancount'),
+      data_sources=data_sources,
+   )
+
+if __name__ == '__main__':
+    run_reconcile(sys.argv[1:])

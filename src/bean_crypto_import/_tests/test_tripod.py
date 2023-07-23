64a9be37afe1fdd4d38cab75eb08e62af86000ce
Eric <ericaltendorf@gmail.com>
2023-07-23 10:45:22 -0700

    Add tests for tripod

---
+++ src/bean_crypto_import/_tests/test_tripod.py
@@ -0,0 +1,124 @@
+from decimal import Decimal
+import sys
+from bean_crypto_import.tripod import Tripod
+
+import pytest
+
+# Valid initialization
+
+def test_valid_transaction():
+    tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                    sent_amt="10000.0", sent_cur="USDT",
+                    fees_amt="10.0", fees_cur="USD")
+    assert tripod.rcvd_amt == Decimal("1.0")
+    assert tripod.rcvd_cur == "BTC"
+    assert tripod.sent_amt == Decimal("10000.0")
+    assert tripod.sent_cur == "USDT"
+    assert tripod.fees_amt == Decimal("10.0")
+    assert tripod.fees_cur == "USD"
+
+def test_valid_send():
+    tripod = Tripod(rcvd_amt="", rcvd_cur="",
+                    sent_amt="100.0", sent_cur="USDT",
+                    fees_amt="1.0", fees_cur="USD")
+    
+    assert tripod.is_transfer()
+    assert tripod.amount() == Decimal("100.0")
+    assert tripod.currency() == "USDT"
+
+def test_valid_receive():
+    tripod = Tripod(rcvd_amt="10.0", rcvd_cur="BTC",
+                    sent_amt="", sent_cur="",
+                    fees_amt="1.0", fees_cur="USD")
+    
+    assert tripod.is_transfer()
+    assert tripod.amount() == Decimal("10.0")
+    assert tripod.currency() == "BTC"
+
+# Invalid initialization
+
+def test_reject_missing_field_of_leg():
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="")
+
+def test_reject_negative_amounts():
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="-1.0", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="-10000.0", sent_cur="USDT",
+                        fees_amt="10.0", fees_cur="USD")
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="-1.0", rcvd_cur="BTC",
+                        sent_amt="10000.0", sent_cur="USDT",
+                        fees_amt="-10.0", fees_cur="USD")
+
+def test_reject_fee_only():
+    with pytest.raises(Exception):
+        tripod = Tripod(fees_amt="10.0", fees_cur="")
+
+def test_reject_for_self_transaction():
+    with pytest.raises(Exception):
+        tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                        sent_amt="1.0", sent_cur="BTC",
+                        fees_amt="10.0", fees_cur="USD")
+
+# Other logic
+
+def test_is_transaction():
+    tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+                    sent_amt="10000.0", sent_cur="USDT",
+                    fees_amt="10.0", fees_cur="USD")
+    assert tripod.is_transaction()
+    assert not tripod.is_transfer()
+    assert tripod.tx_class() == "Transaction"
+
+def test_is_send():
+    tripod = Tripod(rcvd_amt="", rcvd_cur="",
+                    sent_amt="100.0", sent_cur="USDT",
+                    fees_amt="1.0", fees_cur="USD")
+    assert tripod.is_transfer()
+    assert tripod.is_send()
+    assert not tripod.is_receive()
+    assert tripod.tx_class() == "Send"
+
+def test_is_receive():
+    tripod = Tripod(rcvd_amt="10.0", rcvd_cur="BTC",
+                    sent_amt="", sent_cur="",
+                    fees_amt="1.0", fees_cur="USD")
+    assert tripod.is_transfer()
+    assert tripod.is_receive()
+    assert not tripod.is_send()
+    assert tripod.tx_class() == "Receive"
+
+# TODO
+# def test_impute_price():
+#     tripod = Tripod(rcvd_amt="1.0", rcvd_cur="BTC",
+#                     sent_amt="10000.0", sent_cur="USDT",
+#                     fees_amt="10.0", fees_cur="USD")
+#     assert tripod.imputed_price("USDT") == Decimal("10000.0")
+#     assert tripod.imputed_price("BTC") == Decimal("1.0")
--- src/bean_crypto_import/tripod.py
+++ src/bean_crypto_import/tripod.py
@@ -15,9 +15,10 @@ def check_booleq_and_return(val1, val2):
     return bool(val1)
 
 class Tripod():
-    """A tripod has up to three legs: received, sent, and fees.  At least
-       one of {received, sent} is required, and if both are present, then
-       their currencies must differ."""
+    """A tripod represents a transfer with up to three legs (received, sent, and
+    fees), from the perspective of one account.  Therefore, each amount is
+    represented as a positive value.  At least one of {received, sent} is
+    required, and if both are present, then their currencies must differ."""
     def __init__(self,
                  rcvd_amt: str, rcvd_cur: str,
                  sent_amt: str, sent_cur: str,
@@ -33,6 +34,15 @@ class Tripod():
         self.fees_amt: Decimal = D(fees_amt)
         self.fees_cur: str = fees_cur
 
+        if self.rcvd_amt < 0:
+            raise Exception("Negative received amount")
+        if self.sent_amt < 0:
+            raise Exception("Negative received amount")
+        if self.fees_amt < 0:
+            raise Exception("Negative received amount")
+
+        # This is here because we currently have no fee-only transactions,
+        # but it could be removed, e.g., for account maintenance fees.
         if not self.rcvd and not self.sent:
             raise Exception("Must have recieved and/or sent")
         
@@ -41,8 +51,17 @@ class Tripod():
                             "received) but sent and received currencies are the "
                             f"same: {self.rcvd_cur}")
 
-    def is_transaction(self):
+    def is_transaction(self) -> bool:
         return self.rcvd and self.sent
+
+    def is_transfer(self) -> bool:
+        return not self.is_transaction()
+
+    def is_send(self) -> bool:
+        return self.is_transfer() and self.sent
+
+    def is_receive(self) -> bool:
+        return self.is_transfer() and self.rcvd
 
     # TODO: maybe need a flag to say whether to subtract out fee amounts or not
     def imputed_price(self, target_currency) -> Decimal:
@@ -58,21 +77,21 @@ class Tripod():
                 f"doesn't match sent currency {self.sent_cur}" \
                 f"or received currency {self.rcvd_cur}")
 
-    def is_transfer(self):
-        return not self.is_transaction()
-
-    def currency(self):
+    def currency(self) -> str:
         if not self.is_transfer():
             raise Exception("Single currency only defined on transfers")
         return self.rcvd_cur or self.sent_cur
     
-    def amount(self):
+    def amount(self) -> Decimal:
         if not self.is_transfer():
             raise Exception("Single amount only defined on transfers")
         return self.rcvd_amt or self.sent_amt
 
-    def description(self):
+    def tx_class(self) -> str:
         if self.is_transaction():
             return "Transaction"
         elif self.is_transfer():
-            return "Transfer"
+            if self.sent_amt:
+                return "Send"
+            else:
+                return "Receive"

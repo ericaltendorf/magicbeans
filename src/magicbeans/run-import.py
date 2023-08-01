74dae3333533b128dea491529d6d6ae8bfa0856d
Eric <ericaltendorf@gmail.com>
2023-08-01 10:54:08 -0700

    Move account setup stuff into config (not in git)

--- src/magicbeans/run-import.py
+++ src/magicbeans/run-import.py
@@ -14,53 +14,10 @@ from magicbeans.importers.gateio import GateIOImporter
 import beangulp
 from beangulp.importer import Importer
 
-def get_importers(network: transfers.Network) -> List[Importer]:
-   importers = []
-
-   # TODO: move these arguments to a config file
-   importers.append(CoinbaseImporter(
-      account_root="Assets:Coinbase",
-      account_gains="Income:PnL",
-      account_fees="Expenses:Financial:Fees",
-      network=network))
-
-   importers.append(ChiaWalletImporter(
-      account_root="Assets:ChiaWallet",
-      account_mining_income="Income:Mining",
-      account_gains="Income:PnL",
-      account_fees="Expenses:Financial:Fees",
-      network=network))
-
-   importers.append(GateIOImporter(
-      account_root="Assets:GateIO",
-      account_pnl="Income:PnL",
-      account_fees="Expenses:Financial:Fees",
-      network=network))
-
-   importers.append(CoinbaseProImporter(
-      account_root="Assets:Coinbase",
-      account_pnl="Income:PnL",
-      account_fees="Expenses:Financial:Fees",
-      network=network))
-
-   return importers
-
 if __name__ == '__main__':
-    network = transfers.Network([
-        transfers.Link("Coinbase", "Bank", "USD"),
-        transfers.Link("Coinbase", "GateIO", "USDT"),
-        transfers.Link("Coinbase", "Ledger", "BTC"),
-        transfers.Link("Coinbase", "Ledger", "ETH"),
-        transfers.Link("Coinbase", "Ledger", "LTC"),
-        transfers.Link("GateIO", "ChiaWallet", "XCH"),
-    ], ["Bank", "Ledger"]) 
-
-    importers = get_importers(network)
-
-    hooks = [config.cbp_filter_xfer_hook,
-             config.chiawallet_filter_xfer_hook,
-             config.cbp_tweak_xfer_timestamp_hook,
-             ]
+    network = config.get_network()
+    importers = config.get_importers(network)
+    hooks = config.get_hooks()
 
     cli = beangulp.Ingest(importers, hooks).cli
     cli()

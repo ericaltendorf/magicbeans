3ad6b532b4002477ffd13f4d6093f7200d6052bc
Eric <ericaltendorf@gmail.com>
2023-08-01 11:27:20 -0700

    clean up imports and unneeded 'cli'

--- src/magicbeans/run-import.py
+++ src/magicbeans/run-import.py
@@ -1,23 +1,14 @@
 """Run importers and reconcile data, using the beancount-import framework
    (https://github.com/jbms/beancount-import)."""
 
-import glob
-import os
-import json
-import sys
 from typing import List
-from magicbeans import config, transfers
-from magicbeans.importers.chiawallet import ChiaWalletImporter
-from magicbeans.importers.coinbase import CoinbaseImporter
-from magicbeans.importers.coinbasepro import CoinbaseProImporter
-from magicbeans.importers.gateio import GateIOImporter
+from magicbeans import config
 import beangulp
-from beangulp.importer import Importer
 
 if __name__ == '__main__':
-    network = config.get_network()
-    importers = config.get_importers(network)
-    hooks = config.get_hooks()
+   network = config.get_network()
+   importers = config.get_importers(network)
+   hooks = config.get_hooks()
 
-    cli = beangulp.Ingest(importers, hooks).cli
-    cli()
+   ingest = beangulp.Ingest(importers, hooks)
+   ingest()

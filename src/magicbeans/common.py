49655505a097bad0888cd19dd563b8ec332ec871
Eric <ericaltendorf@gmail.com>
2023-08-25 16:14:33 -0700

    Update transfer cost-spec function

--- src/magicbeans/common.py
+++ src/magicbeans/common.py
@@ -99,10 +99,13 @@ def split_out_marked_fees(entry: Transaction, pnl_account) -> Tuple[Transaction,
 
         return (new_txn, fee_txn)
 
-# Need to add a USD cost spec on transfers, see
+# We used to need to add a USD cost spec on transfers, see
 #   https://github.com/beancount/beancount/issues/476
+# With our fix to cost bucketing (not yet checked into beancount head)
+# that is no longer needed; we just use an empty cost spec for transfers.
+# TODO: rename or remove this function.
 def usd_cost_spec(transferred_currency):
     if transferred_currency == "USD":
         return None  # No cost basis needed to track
     
-    return position.CostSpec(None, None, "USD", None, None, None)
+    return position.CostSpec(None, None, None, None, None, None)

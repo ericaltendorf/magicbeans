fca9394545b7cd7566f8e17360f03a1eb95a7af8
Eric Altendorf <ericaltendorf@gmail.com>
2024-08-14 11:48:07 -0700

    test: add acquisitions test; currently fails

--- src/magicbeans/_tests/test_disposals.py
+++ src/magicbeans/_tests/test_disposals.py
@@ -16,8 +16,6 @@ def usd_cost(amount: str, year: int):
 def nyd(year: int):
     return datetime.date(year, 1, 1)
 
-# TODO: add test for acquisition lots
-
 def test_lotindex_from_inventories():
     inventory_blocks = [
         InventoryBlock('BTC', 'Assets:MtGox', [
@@ -49,4 +47,42 @@ def test_lotindex_from_inventories():
     # These should have been assigned IDs
     assert lotindex.get_lotid('BTC', usd_cost('2000.0', 2016)) == 1
     assert lotindex.get_lotid('BTC', usd_cost('8000.0', 2020)) == 2
+
+def test_lotindex_from_acquisitions():
+    acquisitions = [
+        Transaction({}, nyd(2015), None, None, None, None, None, [
+            Posting('Assets:MtGox',
+                    Amount(D('1.8'), 'BTC'),
+                    usd_cost('1000.0', 2015),
+                    None, None, None),
+            Posting('Assets:Bank', Amount(D('-1800'), 'USD'), None, None, None, None),
+        ]),
+        Transaction({}, nyd(2016), None, None, None, None, None, [
+            Posting('Assets:MtGox',
+                    Amount(D('0.2'), 'BTC'),
+                    # TODO: is this realistic the cost is here?  it's not inferred from the other leg?
+                    usd_cost('2000.0', 2016),
+                    None, None, None),
+            Posting('Assets:Bank', Amount(D('-6000'), 'USD'), None, None, None, None),
+        ]),
+    ]
+
+    disposals = [
+        Transaction({}, nyd(2022), None, None, None, None, None, [
+            Posting('Assets:MtGox',
+                    Amount(D('-0.1'), 'BTC'),
+                    usd_cost('2000.0', 2016),
+                    None, None, None),
+            Posting('Assets:Bank', Amount(D('6000'), 'USD'), None, None, None, None),
+        ]),
+    ]
+
+    lotindex = LotIndex([], acquisitions, disposals, "USD")
+
+    print(lotindex.debug_str())
+
+    # This should have been assigned an ID
+    assert lotindex.get_lotid('BTC', usd_cost('2000.0', 2016)) == 2
+ 
+
 
